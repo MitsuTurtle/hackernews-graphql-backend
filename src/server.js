9691a94beac0625a0628cfc507f965adcc0e36a8
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-03-31 17:30:14 +0900

    スキーマ定義をHackerNews用に拡張

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// HackerNews1つ1つの投稿
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQLチュートリアルをUdemyで学ぶ',
+    url: 'www.udemy-graphql-tutorial.com',
+  },
+];
+
 // GraphQLスキーマの定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
@@ -11,6 +27,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     info: () => 'HackerNewsクローン',
+    feed: () => links,
   },
 };
 
