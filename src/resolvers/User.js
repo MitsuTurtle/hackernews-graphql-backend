6df68ddcc9228b2f657718f0516b5dc794a9925e
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-04-13 18:41:32 +0900

    Userリゾルバの実装

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,7 +1,11 @@
-function feed(parent, args, context) {
-  return context.prisma.link.findMany();
+function links(parent, args, context) {
+  return context.prisma.user
+    .findUnique({
+      where: { id: parent.id },
+    })
+    .links();
 }
 
 module.exports = {
-  feed,
+  links,
 };
